5326e1db60907f10d3ceab6d07c08687908c174e
Regige <144334703+Regige@users.noreply.github.com>
2024-07-12 18:08:25 +0200

    Added model + serializer

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from datetime import date
 
-# Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateField(default=date.today)
+    checked = models.BooleanField(default=False)
+    
+    def __str__(self):
+        # return str(self.id) + ' ' + self.title oder
+        return f'({self.id}) {self.title}'
